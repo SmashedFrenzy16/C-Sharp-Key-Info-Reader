bb1b0c7e71f585a5bb40189e757eeb4333f12176
SmashedFrenzy16 <smashedfrenzy16@gmail.com>
2023-11-10 12:01:40 +0000

    Add program

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.ForegroundColor = ConsoleColor.DarkRed;
+
+string repeat = "Y";
+
+int x_pos = 0;
+do
+{
+
+    Console.SetCursorPosition(x_pos, 0);
+
+    ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+
+    Console.WriteLine($"Key Char: {keyInfo.KeyChar}");
+
+    Console.SetCursorPosition(0, 2);
+
+    Console.WriteLine($"Key: {keyInfo.Key.ToString()}");
+    Console.WriteLine($"Key: {keyInfo.Modifiers.ToString()}");
+
+    Console.Write("Do you want to find the information of another key? (y/n): ");
+
+    repeat = Console.ReadLine();
+
+} while (repeat == "Y" || repeat == "y");
